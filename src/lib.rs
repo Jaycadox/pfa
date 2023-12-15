850e19c10e2da2b7c5d56e73512985dd2b42246a
Jaycadox <>
2023-12-16 01:26:49 +1000

    restructure & add testing

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,54 @@ pub enum PfaError {
     #[error("Unknown PFA error")]
     Unknown,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::{Cursor, Write};
+
+    use crate::{builder::PfaBuilder, reader::PfaReader};
+
+    #[test]
+    fn test_1() {
+        let mut builder = PfaBuilder::new("epic_name");
+        builder
+            .add_file("dir_name/file.txt", vec![1, 2, 3, 4, 5, 6])
+            .unwrap();
+        builder
+            .add_file("dir_name/file2.txt", vec![1, 2, 3, 4, 5, 7])
+            .unwrap();
+        builder
+            .add_file("dir_name/dir/file3.txt", vec![1, 2, 3, 4, 5, 7])
+            .unwrap();
+
+        let bytes = builder.build().unwrap();
+        let mut f = std::fs::File::create("out.pfa").unwrap();
+        f.write_all(&bytes).unwrap();
+        let mut reader = PfaReader::new(Cursor::new(bytes)).unwrap();
+        let mut files = vec![];
+        reader.traverse_files("/", |file| {
+            files.push(file);
+        });
+
+        assert_eq!(&reader.get_name(), &"epic_name");
+        assert_eq!(reader.get_version(), 1);
+        assert_eq!(reader.get_extra_data().len(), 0);
+
+        let f = files.pop().unwrap();
+        assert_eq!(&f.get_name(), "file3.txt");
+        assert_eq!(&f.get_path().to_string(), "/dir_name/dir/file3.txt");
+        assert_eq!(f.get_contents(), &[1, 2, 3, 4, 5, 7]);
+
+        let f = files.pop().unwrap();
+        assert_eq!(&f.get_name(), "file2.txt");
+        assert_eq!(&f.get_path().to_string(), "/dir_name/file2.txt");
+        assert_eq!(f.get_contents(), &[1, 2, 3, 4, 5, 7]);
+
+        let f = files.pop().unwrap();
+        assert_eq!(&f.get_name(), "file.txt");
+        assert_eq!(&f.get_path().to_string(), "/dir_name/file.txt");
+        assert_eq!(f.get_contents(), &[1, 2, 3, 4, 5, 6]);
+
+        assert!(files.is_empty());
+    }
+}
