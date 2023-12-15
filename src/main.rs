f328ea6acaa40fca81319e1ae921d755cab51922
Jaycadox <>
2023-12-16 00:30:25 +1000

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use pfa::builder::*;
 use pfa::reader::*;
 use std::io::Cursor;
+use std::io::Write;
 
 fn main() {
     let mut builder = PfaBuilder::new("epic_name");
@@ -10,9 +11,15 @@ fn main() {
     builder
         .add_file("dir_name/file2.txt", vec![1, 2, 3, 4, 5, 7])
         .unwrap();
+    builder
+        .add_file("dir_name/dir/file3.txt", vec![1, 2, 3, 4, 5, 7])
+        .unwrap();
 
     let bytes = builder.build().unwrap();
+    let mut f = std::fs::File::create("out").unwrap();
+    f.write_all(&bytes).unwrap();
     let mut reader = PfaReader::new(Cursor::new(bytes)).unwrap();
-    let file = reader.get_file("/dir_name2/file2.txt");
-    println!("got file: {:?}", file);
+    reader.traverse_files("/", |file| {
+        println!("Found file: {}", file.get_path());
+    })
 }
